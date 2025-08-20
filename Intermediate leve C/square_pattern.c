73d0b91f656c8969124e7129a17ddd42690ef1f8
Mareedu Naga Venkata Sai Naveen <153056579+MareeduSaiNaveen@users.noreply.github.com>
2025-08-20 11:21:50 +0530

    Add files via upload

---
+++ Intermediate leve C/square_pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int row, col, i, j;
+	printf("Enter a row : ");
+	scanf("%d", &row);
+	printf("Enter a col : ");
+	scanf("%d", &col);
+	for(i = 0; i < row; i++){
+		for(j = 0; j < col; j++){
+			if(i == 0 || i == row-1 || j == 0 || j == col -1){
+				printf("* ");
+			}else{
+				printf("  ");
+			}
+		}printf("\n");
+	}return 0;
+}
